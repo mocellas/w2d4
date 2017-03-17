19f56b81587a3548b8c86d2e713aa2fb395716dd
mocellas <fernando.mocellin@gmail.com>
2017-03-17 17:25:26 +0000

    Moving from old server to the new server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const morgan = require('morgan')
 
 const app = express()
-const PORT = process.env.PORT || 8000; // default port 8000
+const PORT = process.env.PORT || 8080; // default port 8080
 
 // parse application/x-www-form-urlencoded form data into req.body
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,13 +11,33 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(morgan('dev'));
 
 const data = {
   users: [
     { username: 'monica', password: 'testing' },
     { username: 'khurram', password: 'testing2' }
   ]
+}
+
+function randomUsers(len)
+{
+  var text = " ";
+
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < len; i++ )
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
+
+console.log(randomUsers(6));
+
+const users = {
+  "userRandomID": { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur"},
+  "user2RandomID": { id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk"}
 }
 
 function attemptLogin(username, password) {
@@ -77,3 +98,91 @@ app.get("/logout", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//FROM OLD SERVER
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+function stringGen(len)
+{
+  var text = " ";
+
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < len; i++ )
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
+console.log(stringGen(6));
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+//W2D3
+app.get("/urls/:id", (req, res) => {
+  let longUrl = urlDatabase[req.params.id]
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase, longUrl: longUrl};
+  console.log(longUrl)
+  console.log(req.params.id)
+  //res.send("Ok");
+  res.render("urls_show", templateVars);
+});
+//W2D3
+//const bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  var randomURL = stringGen(6);
+  urlDatabase[randomURL] = req.body.longURL;
+
+  // console.log(req.body.longURL);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//W2D3 - Delete route
+app.post("/urls/:id/delete", (req, res) => {
+  var deleteID = req.params.id;
+  delete urlDatabase[deleteID];
+  res.render('/urls_show');
+});
+
+//W2D3 - update route
+app.post("/urls/:id/update", (req, res) => {
+  var updateID = req.params.id;
+  //update urlDatabase[updateID];
+  res.redirect('/urls_show');
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}!`);
+// });
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(urlDatabase)
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
+
