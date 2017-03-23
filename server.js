17a7ec488a580cc78dfe0386227ccd723a93c8a1
mocellas <fernando.mocellin@gmail.com>
2017-03-23 03:12:41 +0000

    Fix for Bug05_Medium

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const morgan = require('morgan')
-
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const app = express()
 const PORT = process.env.PORT || 8080; // default port 8080
-
-// parse application/x-www-form-urlencoded form data into req.body
-app.use(bodyParser.urlencoded({ extended: false }))
-
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-app.set("view engine", "ejs");
-app.use(morgan('dev'));
-
 const data = {
   users: [
     { username: 'monica', password: 'testing' },
     { username: 'khurram', password: 'testing2' }
   ]
 }
+const users = {
+  "userRandomID": { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur"},
+  "user2RandomID": { id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk"}
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.use(morgan('dev'));
 
 function randomUsers(len)
 {
@@ -33,12 +33,6 @@ function randomUsers(len)
   return text;
 }
 
-console.log(randomUsers(6));
-
-const users = {
-  "userRandomID": { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur"},
-  "user2RandomID": { id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk"}
-}
 
 function attemptLogin(username, password) {
   for (user of data.users) {
@@ -46,6 +40,18 @@ function attemptLogin(username, password) {
       return user;
     }
   }
+}
+
+function stringGen(len)
+{
+  var text = " ";
+
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < len; i++ )
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
 }
 
 // Show the treasure if they are logged in, otherwise redirect to LOGIN page.
@@ -61,13 +67,9 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login")
 })
-
 app.post("/login", (req, res) => {
-  //const username = req.body.username;
-  //const password = req.body.password;
-  bcrypt.compareSync("req.body.username", hashed_password); // returns true
-  bcrypt.compareSync("req.body.password", hashed_password); // returns false
-
+  const username = req.body.username;
+  const password = req.body.password;
   const user = attemptLogin(username, password);
 
   if (user) {
@@ -84,13 +86,10 @@ app.post("/login", (req, res) => {
 app.get("/signup", (req, res) => {
   res.render("signup")
 })
-
 app.post("/signup", (req, res) => {
-  const bcrypt = require('bcrypt');
   const password = "req.body.password"; // you will probably this from req.params
   const hashed_password = bcrypt.hashSync(password, 10);
   const username = req.body.username;
-  //const password = req.body.password;
   data.users.push({username: username, password: password});
   res.cookie('username', username);
   res.redirect('/');
@@ -111,19 +110,6 @@ let urlDatabase = {
   "b2xVn2": {"userID": "temp", "URL": "http://www.lighthouselabs.com"},
   "9sm5xK": {"userID": "temp", "URL": "http://www.google.com"}
 };
-
-function stringGen(len)
-{
-  var text = " ";
-
-  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-
-  for( var i=0; i < len; i++ )
-    text += charset.charAt(Math.floor(Math.random() * charset.length));
-
-  return text;
-}
-console.log(stringGen(6));
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -158,20 +144,14 @@ app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase, longUrl: longUrl};
   console.log(longUrl)
   console.log(req.params.id)
-  //res.send("Ok");
   res.render("urls_show", templateVars);
 });
 
-//W2D3
-//const bodyParser = require("body-parser");
-//app.use(bodyParser.urlencoded({extended: true}));
 app.post("/urls", (req, res) => {
   var randomURL = stringGen(6);
-  // urlDatabase[randomURL] = req.body.longURL;
   urlDatabase[randomURL] = { "userID": req.cookies.username,
   "URL": req.body.longURL};
-  // console.log(req.body.longURL);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.render("/urls");
 });
 
 //W2D3 - delete route
@@ -191,7 +171,7 @@ app.post("/urls/:id/delete", (req, res) => {
 
 //W2D3 - update route
 app.post("/urls/:id/update", (req, res) => {
-  var updateID = req.params.id;
+  var updateID = [req.params.shortUTL].URL;
   if (req.cookies.username){
     res.render("urls_new");
   }else{
@@ -202,7 +182,7 @@ app.post("/urls/:id/update", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   console.log('urlDatabase', urlDatabase)
-  let longURL = urlDatabase[req.params.shortURL].URL //replace it by [req.params.id]
+  let longURL = urlDatabase[req.params.shortURL].URL
   console.log('LONGURL', longURL);
   res.redirect(longURL);
 });
